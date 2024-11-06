0c1826f9a10804d7032d11911763580b608354dd
Luke Thompson <luke.thompson@nine.com.au>
2024-11-07 10:14:11 +1100

    changed video ref

--- src/components/blocks/CombinedAnimations/index.tsx
+++ src/components/blocks/CombinedAnimations/index.tsx
@@ -3,7 +3,7 @@
 import ClipPathAnimation from "../../miscellaneous/ClipPathAnimation"
 import TextAnimationWithScroll from "../../miscellaneous/TextAnimationWithScroll"
 // import video from "./src/a"
-// // import video from "../../../assets/videos/abf9ac84-a77ef1de.mp4"
+import video from "../../../../public/assets/videos/abf9ac84-a77ef1de.mp4"
 import { images } from "../../../../public/assets/data/slides"
 import ImageGalleryAnimation from "../../miscellaneous/ImageGalleryAnimation"
 
@@ -34,10 +34,7 @@ const CombinedAnimations: React.FC = () => {
           className='text-[200px] leading-[300px] text-white uppercase font-bold ease-out font-anton tracking-[10px]'
         />
         <video autoPlay muted loop width='100%'>
-          <source
-            src={"/assets/videos/abf9ac84-a77ef1de.mp4"}
-            type='video/mp4'
-          />
+          <source src={video} type='video/mp4' />
         </video>
       </ClipPathAnimation>
       <TextAnimationWithScroll
@@ -70,10 +67,7 @@ const CombinedAnimations: React.FC = () => {
           className='text-[250px] leading-[300px] text-emerald-200 uppercase font-bold ease-out font-anton tracking-[10px]'
         />
         <video autoPlay muted loop width='100%'>
-          <source
-            src={"/assets/videos/abf9ac84-a77ef1de.mp4"}
-            type='video/mp4'
-          />
+          <source src={video} type='video/mp4' />
         </video>
       </ClipPathAnimation>
     </section>

6f82f549862e59fb7ac293da86a066f9e5a349c2
Luke Thompson <luke.thompson@nine.com.au>
2024-11-07 10:01:03 +1100

    added public folder

--- src/components/blocks/CombinedAnimations/index.tsx
+++ src/components/blocks/CombinedAnimations/index.tsx
@@ -34,10 +34,7 @@ const CombinedAnimations: React.FC = () => {
           className='text-[200px] leading-[300px] text-white uppercase font-bold ease-out font-anton tracking-[10px]'
         />
         <video autoPlay muted loop width='100%'>
-          <source
-            src={"src/assets/videos/abf9ac84-a77ef1de.mp4"}
-            type='video/mp4'
-          />
+          <source src={"./src/public/abf9ac84-a77ef1de.mp4"} type='video/mp4' />
         </video>
       </ClipPathAnimation>
       <TextAnimationWithScroll
@@ -70,10 +67,7 @@ const CombinedAnimations: React.FC = () => {
           className='text-[250px] leading-[300px] text-emerald-200 uppercase font-bold ease-out font-anton tracking-[10px]'
         />
         <video autoPlay muted loop width='100%'>
-          <source
-            src={"/src/assets/videos/abf9ac84-a77ef1de.mp4"}
-            type='video/mp4'
-          />
+          <source src={"./src/public/abf9ac84-a77ef1de.mp4"} type='video/mp4' />
         </video>
       </ClipPathAnimation>
     </section>

016c6bdc1cc8a1a8b1b620581c4d87256a1142df
Luke Thompson <luke.thompson@nine.com.au>
2025-03-05 11:19:58 +1100

    shiny

--- src/components/Effects/CarScene/CarScene.tsx
+++ src/components/Effects/CarScene/CarScene.tsx
@@ -2,20 +2,26 @@ import * as THREE from "three"
 import { Canvas, useThree } from "@react-three/fiber"
 import { KTX2Loader } from "three-stdlib"
 import { Gltf, Environment, CameraControls } from "@react-three/drei"
+import { Suspense } from "react"
 
 const CarScene: React.FC = () => {
   return (
     <div className='h-screen w-screen'>
       <Canvas camera={{ position: [-1, 0.5, 3] }} className='h-full w-full'>
-        <Scene />
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
 
+        {/* Custom environment background */}
         <Environment background>
           <mesh scale={100}>
             <sphereGeometry args={[1, 64, 64]} />
             <meshBasicMaterial color='#393939' side={THREE.BackSide} />
           </mesh>
         </Environment>
+
         <ambientLight intensity={0.5} />
+        <directionalLight intensity={0.6} position={[5, 5, 5]} />
 
         <CameraControls />
       </Canvas>
@@ -39,6 +45,10 @@ function Scene() {
           loader.setKTX2Loader(ktx2Loader.detectSupport(gl))
         }}
       />
+
+      {/* Environment with fallback */}
+      <Environment preset='sunset' background blur={0.5} />
+
       <Gltf
         src={"/assets/images/scene.glb"}
         extendLoader={(loader) => {
